596d9bc37a36c88329a91cd93b2d2fd39bc18b75
yeasar <syeasar.kuet@gmail.com>
2025-10-18 16:32:55 +0600

    :ghost: chores: Update footer

--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ export default function Page() {
           </Link>
           <span>·</span>
           <Link
-            href="https://github.com/syeasar/word-walk"
+            href="https://github.com/syeasar/wordwalk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex items-center gap-1 underline hover:opacity-80"
